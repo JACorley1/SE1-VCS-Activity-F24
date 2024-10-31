551ea387e7aaf73e6a2e47232a5e993cca7c85e9
{Mughki} <{bluntsam23@hotmail.com}>
2024-10-30 20:09:58 -0400

    Implement Pig Class and Basic Unit Tests
    Implemented Pig data class with two fields, age and size, along with a two param constructor. Added basic unit tests for Pig constructor.

--- code/Zodiac Animals/src/edu/westga/cs3211/zodiac_animals/model/Pig.java
+++ code/Zodiac Animals/src/edu/westga/cs3211/zodiac_animals/model/Pig.java
@@ -1,5 +1,35 @@
 package edu.westga.cs3211.zodiac_animals.model;
 
 public class Pig {
-
+	int age;
+	int size;
+	
+	public Pig(int age, int size) {
+		this.setAge(age);
+		this.setSize(size);
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int age) {
+		if (age < 0) {
+			throw new IllegalArgumentException("Pig cannot be younger than 0.");
+		}
+		
+		this.age = age;
+	}
+	
+	public int getSize() {
+		return this.size;
+	}
+	
+	public void setSize(int size) {	
+		if (size < 0) {
+			throw new IllegalArgumentException("Pig cannot be smaller than 0.");
+		}
+		
+		this.size = size;
+	}
 }
